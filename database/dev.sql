0e7135eef5afe85cb7c62fd4fd88f4fbb17f30ff
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-11-26 23:55:03 -0300

    Create database files

---
+++ database/dev.sql
@@ -0,0 +1,29 @@
+DROP SCHEMA IF EXISTS emotion_recog;
+
+CREATE SCHEMA emotion_recog;
+
+USE emotion_recog;
+
+CREATE TABLE tb_emocao (
+    id INT PRIMARY KEY auto_increment,
+    nome VARCHAR(45) NOT NULL,
+    hex_color CHAR(6) NOT NULL
+);
+
+CREATE TABLE tb_historico (
+    id INT PRIMARY KEY auto_increment,
+    user_token VARCHAR(36) NOT NULL, -- UUID()
+    fk_emocao INT NOT NULL,
+    dt_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (fk_emocao) REFERENCES tb_emocao (id),
+);
+
+-- Dados para emoções
+INSERT INTO tb_emocao (nome, hex_color) VALUES
+    ('Sad', '0000FF'),       -- Blue for sadness
+    ('Happy', 'FFFF00'),     -- Yellow for happiness
+    ('Angry', 'FF0000'),     -- Red for anger
+    ('Surprise', 'FFA500'),  -- Orange for surprise
+    ('Fear', '800080'),      -- Purple for fear
+    ('Disgust', '008000'),   -- Green for disgust
+    ('Neutral', '808080');   -- Gray for neutrality
