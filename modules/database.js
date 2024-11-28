16f3e97facf5afd6f349ca37ff381127c6910a0a
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-11-28 01:13:31 -0300

    Rename database names to match railway

--- modules/database.js
+++ modules/database.js
@@ -8,10 +8,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 const pool = mysql
     .createPool({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        port: process.env.MYSQL_PORT || '3306',
+        host: process.env.MYSQLHOST,
+        user: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASSWORD,
+        port: process.env.MYSQLPORT || '3306',
         database: process.env.MYSQL_DATABASE,
     })
     .promise();
